8a7fb97771a2a2ad1bcc39c304f47451b709e82a
jhu1 <jhu1@i9i8.com>
2019-04-23 11:30:09 +0800

    update

---
+++ floryday/nginx/init.sh
@@ -0,0 +1,11 @@
+ln -sf /usr/share/nginx/html/vb/floryday/src/php/webapp /usr/share/nginx/html/vb/floryday/src/php/webapp/en && \
+ln -sf /usr/share/nginx/html/vb/floryday/src/php/webapp /usr/share/nginx/html/vb/floryday/src/php/webapp/de && \
+ln -sf /usr/share/nginx/html/vb/floryday/src/php/webapp /usr/share/nginx/html/vb/floryday/src/php/webapp/se && \
+ln -sf /usr/share/nginx/html/vb/floryday/src/php/webapp /usr/share/nginx/html/vb/floryday/src/php/webapp/fr && \
+ln -sf /usr/share/nginx/html/vb/floryday/src/php/webapp /usr/share/nginx/html/vb/floryday/src/php/webapp/no && \
+ln -sf /usr/share/nginx/html/vb/floryday/src/php/webapp /usr/share/nginx/html/vb/floryday/src/php/webapp/da && \
+ln -sf /usr/share/nginx/html/vb/floryday/src/php/webapp /usr/share/nginx/html/vb/floryday/src/php/webapp/fi && \
+ln -sf /usr/share/nginx/html/vb/floryday/src/php/webapp /usr/share/nginx/html/vb/floryday/src/php/webapp/ru && \
+ln -sf /usr/share/nginx/html/vb/floryday/src/php/webapp /usr/share/nginx/html/vb/floryday/src/php/webapp/nl && \
+ln -sf /usr/share/nginx/html/vb/floryday/src/php/webapp /usr/share/nginx/html/vb/floryday/src/php/webapp/it && \
+ln -sf /usr/share/nginx/html/vb/floryday/src/php/webapp /usr/share/nginx/html/vb/floryday/src/php/webapp/pt
