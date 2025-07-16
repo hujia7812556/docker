78ee907eb60603e9b153bc2474d96ef7459ce608
hujia <hujia7812556@gmail.com>
2025-07-16 21:10:12 +0800

    迁移

---
+++ common/tencent-lh-sg/postgres/init/init.sql
@@ -0,0 +1,5 @@
+CREATE DATABASE fastkol_fetcher;
+CREATE DATABASE fastkol_admin;
+
+GRANT ALL PRIVILEGES ON DATABASE fastkol_fetcher TO postgres;
+GRANT ALL PRIVILEGES ON DATABASE fastkol_admin TO postgres;
